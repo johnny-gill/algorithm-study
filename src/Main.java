e0129d493b51345639543537d5d9e126e106e6dd
johnny-gill <uint777@gmail.com>
2022-09-23 16:17:40 +0900

    0103

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
         System.out.println(new Main().solution(str));
     }
 
     private String solution(String str) {
+
         String answer = "";
+        int max = Integer.MIN_VALUE;
 
-        for (char c : str.toCharArray()) {
-            answer += Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c);
+        for (String s : str.split(" ")) {
+            int length = s.length();
+            if (length > max) {
+                max = length;
+                answer = s;
+            }
         }
-
-//        int alphabetDiff = 'A' - 'a';
-//        for (char c : str.toCharArray()) {
-//            if ('A' <= c && c <= 'Z') {
-//                answer += (char) (c - alphabetDiff);
-//            } else {
-//                answer += (char) (c + alphabetDiff);
-//            }
-//        }
 
         return answer;
     }
+
+//    private String solution(String str) {
+//
+//        String answer = "";
+//        int max = Integer.MIN_VALUE;
+//
+//        int pos = 0;
+//        while ((pos = str.indexOf(" ")) != -1) {
+//            String tmp = str.substring(0, pos);
+//            int length = tmp.length();
+//            if (max < length) {
+//                max = length;
+//                answer = tmp;
+//            }
+//
+//            str = str.substring(pos + 1);
+//        }
+//
+//        if (max < str.length()) {
+//            answer = str;
+//        }
+//
+//        return answer;
+//    }
 }
