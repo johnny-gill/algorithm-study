3263d99f0eb623fd1e62dee12ce98e0bb9f6b400
whoami <whoami@me.com>
2022-09-23 15:10:59 +0900

    p0101

--- src/p0101.java
+++ src/p0101.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class p0101 {
     public static void main(String[] args) {
-        System.out.println("p0101.main2");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char chr = sc.next().charAt(0);
+
+        System.out.println(new p0101().solution(str, chr));
+    }
+
+    private int solution(String str, char chr) {
+
+        str = str.toLowerCase();
+        chr = Character.toLowerCase(chr);
+
+        int answer = 0;
+        for (char c : str.toCharArray()) {
+            if (c == chr) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
