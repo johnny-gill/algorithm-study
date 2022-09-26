05f266117589015d9261dac1a81731e6cbc94aa0
johnny-gill <uint777@gmail.com>
2022-09-26 17:21:44 +0900

    0111

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(new Main().solution(str));
+        String s = sc.next();
+        System.out.println(new Main().solution(s));
     }
 
-    private Integer solution(String str) {
-        return Integer.parseInt(str.replaceAll("[^0-9]", ""));
+    private String solution(String s) {
+        s += " ";
+        String answer = "";
+        char beforeChar = s.charAt(0);
+        int sum = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (beforeChar == s.charAt(i)) {
+                sum++;
+            } else {
+                answer += beforeChar + "" + ((sum == 1) ? "" : sum);
+                sum = 1;
+                beforeChar = s.charAt(i);
+            }
+        }
+
+        return answer;
     }
 }
