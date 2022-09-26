e25364cd8087e3c2f34cc828da5e9da14bea3343
johnny-gill <uint777@gmail.com>
2022-09-26 17:23:25 +0900

    0110

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        System.out.println(new Main().solution(s));
-    }
-
-    private String solution(String s) {
-        s += " ";
-        String answer = "";
-        char beforeChar = s.charAt(0);
-        int sum = 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (beforeChar == s.charAt(i)) {
-                sum++;
-            } else {
-                answer += beforeChar + "" + ((sum == 1) ? "" : sum);
-                sum = 1;
-                beforeChar = s.charAt(i);
-            }
+        char t = sc.next().charAt(0);
+        for (int i : new Main().solution(s, t)) {
+            System.out.print(i + " ");
         }
 
+    }
+
+//    teachermode e
+//    edomrehcaet e
+//    1 0 1 2 1 0 1 2 2 1 0
+    private int[] solution(String s, char t) {
+        int[] answer = new int[s.length()];
+        boolean findLeftIndex = false;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                rightIndex = i + 1;
+                leftIndex = i;
+                findLeftIndex = true;
+            } else {
+                if (!findLeftIndex) { // 왼쪽에 t가 없으면 오른쪽 t와의 거리 계산
+                    answer[i] = s.indexOf(t, rightIndex) - i;
+                } else if (s.indexOf(t, rightIndex) == -1) { // 오른쪽에 t가 없으면 왼쪽 t와의 거리 계산
+                    answer[i] = i - leftIndex;
+                } else {
+                    answer[i] = Math.min(i - leftIndex, s.indexOf(t, rightIndex) - i);
+                }
+            }
+        }
         return answer;
     }
+
+
 }
