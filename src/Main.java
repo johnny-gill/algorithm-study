c5c6f71f1ecbd5cd23f8c960a589bdf874ca5b83
johnny-gill <uint777@gmail.com>
2022-09-26 14:29:29 +0900

    0108

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
         System.out.println(new Main().solution(str));
     }
 
     private String solution(String str) {
+        str = str.replaceAll("[^a-zA-Z]", "");
         return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString()) ? "YES" : "NO";
     }
-
-//    private String solution(String str) {
-//        String answer = "YES";
-//
-//        str = str.toLowerCase();
-//        int len = str.length() / 2;
-//        int back = str.length() - 1;
-//        for (int i = 0; i < len; i++) {
-//            if (str.charAt(i) != str.charAt(back - i)) {
-//                answer = "NO";
-//                break;
-//            }
-//        }
-//
-//        return answer;
-//    }
-
 }
