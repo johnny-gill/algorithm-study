bf5efe840156fff78827d8e07f0a71ba180d4135
johnny-gill <uint777@gmail.com>
2022-09-27 14:11:16 +0900

    0205

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        new Main().solution(n);
+        System.out.println(new Main().solution(n));
     }
 
-    private void solution(int n) {
-        int[] arr = new int[n];
-        arr[0] = arr[1] = 1;
+    private int solution(int n) {
+        int answer = 0;
+        boolean[] prime = new boolean[n + 1];
+        prime[0] = prime[1] = true;
 
-        System.out.print("1 1 ");
-        for (int i = 2; i < n; i++) {
-            arr[i] = arr[i - 2] + arr[i - 1];
-            System.out.print(arr[i] + " ");
+        for (int i = 2; i <= n; i++) {
+            if (!prime[i]) {
+                answer++;
+                for (int j = i; j <= n; j+=i) {
+                    prime[j] = true;
+                }
+            }
         }
+
+        return answer;
     }
 }
