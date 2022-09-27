2896b6bc73b2cb1e2345be5414137e4dba61ca3d
johnny-gill <uint777@gmail.com>
2022-09-27 10:53:35 +0900

    0203

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.println(new Main().solution(n, arr));
-    }
-
-    private int solution(int n, int[] arr) {
-        int answer = 1;
-        int maxHeight = arr[0];
-        for (int i = 1; i < n; i++) {
-            if (maxHeight < arr[i]) {
-                maxHeight = arr[i];
-                answer++;
+        int[][] arr = new int[2][n];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
 
-        return answer;
+        new Main().solution(n, arr);
+    }
+
+    private void solution(int n, int[][] arr) {
+        for (int j = 0; j < n; j++) {
+            int a = arr[0][j];
+            int b = arr[1][j];
+
+            if (a == b) {
+                System.out.println("D");
+            } else if ((a == 2 && b == 1) || (a == 1 && b == 3) || (a == 3 && b == 2)) {
+                System.out.println("A");
+            } else {
+                System.out.println("B");
+            }
+        }
     }
 }
