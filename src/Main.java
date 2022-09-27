3a4d6d4e4c1be6c0428c356522c34c7dfd6c1238
johnny-gill <uint777@gmail.com>
2022-09-27 10:15:54 +0900

    0201

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String s = sc.next();
-        System.out.println(new Main().solution(n, s));
-    }
-
-    private String solution(int n, String s) {
-        String answer = "";
-
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
-            s = s.substring(7);
-            int num = Integer.parseInt(tmp, 2);
-            answer += (char) num;
+            arr[i] = sc.nextInt();
         }
 
-        return answer;
+        new Main().solution(n, arr);
     }
 
-//    private String solution(int n, String s) {
-//        String answer = "";
-//
-//        for (int i = 0; i < n; i++) {
-//            String tmp = s.substring(0, 7);
-//            s = s.substring(7);
-//
-//            int decimal = 0;
-//            for (int j = 0; j < 7; j++) {
-//                if (tmp.charAt(j) == '#') {
-//                    decimal += Math.pow(2, 6 - j);
-//                }
-//            }
-//            answer += (char) decimal;
-//        }
-//
-//        return answer;
-//    }
+    private void solution(int n, int[] arr) {
+        System.out.print(arr[0] + " ");
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] < arr[i]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
 }
