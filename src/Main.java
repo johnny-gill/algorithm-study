f2462e073a994bb97825563fd074b419701e2046
johnny-gill <uint777@gmail.com>
2022-09-27 17:30:58 +0900

    0207

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int max = 0;
 
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(new StringBuilder(sc.next()).reverse().toString());
-            arr[i] = num;
-            max = Math.max(max, num);
+            arr[i] = sc.nextInt();
         }
 
-        new Main().solution(n, arr, max);
+        new Main().solution(n, arr);
     }
 
-    private void solution(int n, int[] arr, int max) {
-        boolean[] prime = new boolean[max + 1];
-        prime[0] = prime[1] = true;
-
-        for (int i = 2; i <= max; i++) {
-            if (!prime[i]) {
-                for (int j = i; j <= max; j+=i) {
-                    if (i == j) continue;
-                    prime[j] = true;
-                }
+    private void solution(int n, int[] arr) {
+        int answer = 0;
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                answer += ++cnt;
+            } else {
+                cnt = 0;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (!prime[arr[i]]) {
-                System.out.print(arr[i] + " ");
-            }
-        }
+        System.out.println(answer);
     }
 }
