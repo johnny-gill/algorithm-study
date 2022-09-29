a5ed67bc95c2b71b1833e9771846f38b2a5475dc
johnny-gill <uint777@gmail.com>
2022-09-29 10:03:35 +0900

    0212

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][5];
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int[][] arr = new int[N][M];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        new Main().solution(n, arr);
+        new Main().solution(N, M, arr);
     }
 
-    private void solution(int n, int[][] arr) {
-        int max = Integer.MIN_VALUE;
-        int answer = 0;
+    private void solution(int N, int M, int[][] arr) {
+        boolean[][] student = new boolean[M + 1][M + 1];
 
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                for (int k = 0; k < 5; k++) { // 학년
-                    if (arr[i][k] == arr[j][k]) {
-                        cnt++;
-                        break;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                for (int k = j + 1; k < M; k++) {
+                    int left = arr[i][j];
+                    int right = arr[i][k];
+                    if (!student[left][right]) {
+                        student[left][right] = true;
                     }
                 }
             }
-
-            if (max < cnt) {
-                max = cnt;
-                answer = i + 1;
-            }
         }
 
+        int answer = 0;
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (student[i][j] && !student[j][i]) {
+                    answer++;
+                }
+            }
+        }
         System.out.println(answer);
     }
 }
