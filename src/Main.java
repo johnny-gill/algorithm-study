ab9ddf10f3b11ca7b75b26d8d72a393dabe5ac70
johnny-gill <uint777@gmail.com>
2022-09-29 11:44:50 +0900

    0301

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,54 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-        int[][] arr = new int[N][M];
+        List<Integer> list = new ArrayList<>();
 
+        int N = sc.nextInt();
+        int[] a = new int[N];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = sc.nextInt();
-            }
+            a[i] = sc.nextInt();
         }
 
-        new Main().solution(N, M, arr);
+        int M = sc.nextInt();
+        int[] b = new int[M];
+        for (int i = 0; i < M; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        new Main().solution(N, M, a, b);
     }
 
-    private void solution(int N, int M, int[][] arr) {
-        boolean[][] student = new boolean[M + 1][M + 1];
+    private void solution(int N, int M, int[] a, int[] b) {
+        List<Integer> list = new ArrayList<>();
+        int aIdx = 0;
+        int bIdx = 0;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                for (int k = j + 1; k < M; k++) {
-                    int left = arr[i][j];
-                    int right = arr[i][k];
-                    if (!student[left][right]) {
-                        student[left][right] = true;
-                    }
+        while (true) {
+            if (N == aIdx) {
+                for (int i = bIdx; i < M; i++) {
+                    list.add(b[bIdx++]);
                 }
+                break;
+            }
+
+            if (M == bIdx) {
+                for (int i = aIdx; i < N; i++) {
+                    list.add(a[aIdx++]);
+                }
+                break;
+            }
+
+            if (a[aIdx] < b[bIdx]) {
+                list.add(a[aIdx++]);
+            } else {
+                list.add(b[bIdx++]);
             }
         }
 
-        int answer = 0;
-        for (int i = 1; i <= M; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (student[i][j] && !student[j][i]) {
-                    answer++;
-                }
-            }
+        for (Integer num : list) {
+            System.out.print(num + " ");
         }
-        System.out.println(answer);
     }
 }
