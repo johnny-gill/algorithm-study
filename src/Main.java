242b025fbbf143e2f577ebfede2312afd23aa0a5
johnny-gill <uint777@gmail.com>
2022-10-06 17:10:54 +0900

    0304

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,35 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int K = sc.nextInt();
+        int M = sc.nextInt();
 
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
 
-        new Main().solution(N, K, arr);
+        new Main().solution(N, M, arr);
     }
 
-    private void solution(int N, int K, int[] arr) {
-        int max = 0;
-        int sum = 0;
+    private void solution(int N, int M, int[] arr) {
+        int answer, sum, lt;
+        answer = sum = lt = 0;
 
-        for (int i = 0; i < K; i++) {
-            sum += arr[i];
+        for (int rt = 0; rt < N; rt++) {
+            sum += arr[rt];
+
+            if (sum == M) {
+                answer++;
+            }
+
+            while (sum >= M) {
+                sum -= arr[lt++];
+                if (sum == M) {
+                    answer++;
+                }
+            }
         }
-        max = sum;
 
-        for (int i = K; i < N; i++) {
-            sum = sum - arr[i - K] + arr[i];
-            max = Integer.max(max, sum);
-        }
-
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
