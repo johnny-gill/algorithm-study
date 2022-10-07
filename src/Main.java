4806ae9d340eddbb57e02ae5e8f42a53eb0717b0
johnny-gill <uint777@gmail.com>
2022-10-07 10:24:30 +0900

    0401

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        new Main().solution(N);
+        String str = sc.next();
+
+        new Main().solution(N, str);
     }
 
-    private void solution(int N) {
-        int answer = 0;
-        int lt = 1;
-        int sum = 0;
-        N = N/2 + 1;
+//    private void solution(int n, String str) {
+//        int[] arr = new int[5];
+//
+//        for (char c : str.toCharArray()) {
+//            arr[c - 'A']++;
+//        }
+//
+//        int max = 0;
+//        int answer = 0;
+//        for (int i = 0; i < 5; i++) {
+//            if (max < arr[i]) {
+//                max = arr[i];
+//                answer = i;
+//            }
+//        }
+//
+//        System.out.println(Character.toChars(answer + 'A'));
+//    }
 
-        for (int rt = 1; rt < N; rt++) {
-            sum += rt;
+    private void solution(int n, String str) {
+        Map<Character, Integer> map = new HashMap<>();
 
-            if (sum == N) {
-                answer++;
-                sum -= lt++;
-            }
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
 
-            while (sum > N) {
-                sum -= lt++;
-
-                if (sum == N) {
-                    answer++;
-                    sum -= lt++;
-                }
+        int max = 0;
+        char answer = 0;
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            int curVal = entry.getValue();
+            if (max < curVal) {
+                max = curVal;
+                answer = entry.getKey();
             }
         }
 
