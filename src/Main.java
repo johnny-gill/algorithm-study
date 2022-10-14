2f12d11d6ea25b06419fd89c4e8c918dfac7da92
johnny-gill <uint777@gmail.com>
2022-10-14 17:19:20 +0900

    0404

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-        new Main().solution(N, K, arr);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        new Main().solution(s, t);
     }
 
-    private void solution(int N, int K, int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
+    private void solution(String s, String t) {
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        }
 
+        int cnt = 0;
         int lt = 0;
-        for (int rt = 0; rt < N; rt++) {
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+        int tLength = t.length();
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (int rt = 0; rt < s.length(); rt++) {
+            tMap.put(s.charAt(rt), tMap.getOrDefault(s.charAt(rt), 0) + 1);
+            if (rt - lt + 1 == tLength) {
+                if (sMap.equals(tMap)) {
+                    cnt++;
+                }
 
-            if (rt - lt + 1 == K) {
-                sb.append(map.size());
-                sb.append(" ");
-                map.put(arr[lt], map.get(arr[lt]) - 1);
-                if (map.get(arr[lt]) == 0) {
-                    map.remove(arr[lt]);
+                tMap.put(s.charAt(lt), tMap.get(s.charAt(lt)) - 1);
+                if (tMap.get(s.charAt(lt)) == 0) {
+                    tMap.remove(s.charAt(lt));
                 }
                 lt++;
             }
         }
 
-        System.out.println(sb);
+        System.out.println(cnt);
     }
-
-//    private void solution(int N, int K, int[] arr) {
-//        int lt = 0;
-//        int cnt = 0;
-//        List<Integer> list = new ArrayList<>();
-//        StringBuilder sb = new StringBuilder();
-//
-//        for (int rt = 0; rt < N; rt++) {
-//            if (list.contains(arr[rt])) {
-//                cnt++;
-//            }
-//            list.add(arr[rt]);
-//
-//            if (rt - lt + 1 == K) {
-//                sb.append(K - cnt);
-//                sb.append(" ");
-//                list.remove(lt++);
-//                cnt = 0;
-//            }
-//        }
-//
-//        System.out.println(sb);
-//    }
 }
