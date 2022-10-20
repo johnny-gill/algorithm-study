6a994aab517e02996266b8df1c4952643f16f830
johnny-gill <uint777@gmail.com>
2022-10-20 15:56:45 +0900

    0501

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.println(new Main().solution(N, K, arr));
-
+        String str = sc.next();
+        System.out.println(new Main().solution(str));
     }
 
-    private int solution(int N, int K, int[] arr) {
-        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+    private String solution(String str) {
 
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    int sum = arr[i] + arr[j] + arr[k];
-                    treeSet.add(sum);
-                }
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.empty() || stack.peek() == ')') return "NO";
+                stack.pop();
             }
         }
 
-        int cnt = 0;
-        for (Integer x : treeSet) {
-            cnt++;
-            if (cnt == K) return x;
-        }
-
-        return -1;
+        return stack.size() == 0 ? "YES" : "NO";
     }
 }
