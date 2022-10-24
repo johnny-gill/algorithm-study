5c372d3c85c2837c6e4a3f08f27179dd1a437882
johnny-gill <uint777@gmail.com>
2022-10-24 17:20:48 +0900

    0504

--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str = sc.next();
 
-        int N = sc.nextInt();
-        int[][] board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                board[i][j] = sc.nextInt();
-            }
-        }
+        System.out.println(new Main().solution(str));
 
-        int K = sc.nextInt();
-        int[] moves = new int[K];
-        for (int i = 0; i < K; i++) {
-            moves[i] = sc.nextInt();
-        }
-
-        System.out.println(new Main().solution(N, board, K, moves));
     }
 
-    private int solution(int N, int[][] board, int K, int[] moves) {
+    private int solution(String str) {
         Stack<Integer> stack = new Stack<>();
-        int cnt = 0;
 
-        for (int i = 0; i < K; i++) {
-            int column = moves[i] - 1;
-            for (int j = 0; j < N; j++) {
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                Integer backNum = stack.pop();
+                Integer frontNum = stack.pop();
 
-                int num = board[j][column];
-                if (num != 0) {
-                    if (!stack.empty() && num == stack.peek()) {
-                        cnt += 2;
-                        stack.pop();
-                    } else {
-                        stack.push(num);
-                    }
-                    board[j][column] = 0;
-                    break;
+                if (c == '+') {
+                    stack.push(frontNum + backNum);
+                } else if (c == '-') {
+                    stack.push(frontNum - backNum);
+                } else if (c == '*') {
+                    stack.push(frontNum * backNum);
+                } else if (c == '/') {
+                    stack.push(frontNum / backNum);
                 }
             }
         }
 
-        return cnt;
+        return stack.pop();
     }
 }
